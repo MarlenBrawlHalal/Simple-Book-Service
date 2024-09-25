33bb848481e16773743db8b4c4c79f537b485b5e
biba <aytbayb@inbox.ru>
2024-09-25 20:49:37 +0500

    initial commit

---
+++ src/main/java/com/example/simplebookservice/SimpleBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplebookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBookServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleBookServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/simplebookservice/SimpleBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBookServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
